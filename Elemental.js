9184ffbb04758198d217cebb723a1b24dc52ffe8
klimdanick <github@klimdanick.nl>
2025-01-21 15:30:34 +0100

    cards as classes

--- Elemental.js
+++ Elemental.js
@@ -35,3 +35,38 @@ window.onload = () => {
     Interval = setInterval(() => {if (loadedScripts.length >= 3) {OnElementalLoad(); clearInterval(Interval);}}, 5);
     setTimeout(() => {clearInterval(Interval); if (loadedScripts.length < 3) OnElementalLoad();}, 1000);
 }
+
+class Element {
+    constructor(tagName = "Element") {
+        this.htmlEl = document.createElement(tagName);
+        this.style = this.htmlEl.style;
+    }
+
+    appendChild(el) {
+        if (el instanceof Element)
+            this.htmlEl.appendChild(el.htmlEl);
+        if (typeof el == "string")
+            this.htmlEl.innerHTML += el;
+        return this;
+    }
+}
+
+class Layout extends Element{
+    constructor(direction) {
+        super("Layout");
+        if (direction == "grid") {
+            this.htmlEl.classList.add("grid");
+        } else {
+            this.htmlEl.style.flexDirection = direction;
+        }
+        this.htmlEl.style.height = "100%";
+    }
+
+    setAsMain() {
+        let body = document.getElementsByTagName("body")[0]
+        this.appendChild(body.innerHTML);
+        body.innerHTML = "";
+        body.appendChild(this.htmlEl);
+        return this;
+    }
+}
