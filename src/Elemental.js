7fb51f340d99b12ed3fa20a568bf61dcb0e7ad98
klimdanick <github@klimdanick.nl>
2025-12-20 20:15:44 +0100

    addded form elements

--- src/Elemental.js
+++ src/Elemental.js
@@ -32,7 +32,7 @@ function AttachStyle(src) {
     style.href = src;
 }
 
-const defaultLibURL = ""
+const defaultLibURL = "../.."
 
 const getEjsAsset = (name, libURL = defaultLibURL) => `${defaultLibURL}/assets/${name}.png`;
 
@@ -61,7 +61,7 @@ window.addEventListener("load", (event) => {
     reloadElemental();
 });
 
-const reloadElemental = () => {}
+const reloadElemental = () => { }
 
 class Element {
     constructor({ tag = "Element", id = "", classes = [], attributes = {}, listeners = {} }) {
@@ -165,7 +165,7 @@ class BodyElement extends Element {
 class Layout extends Element {
 
     constructor({ tag = "Layout", id = "", classes = [], attributes = {}, listeners = {} } = {}) {
-        super({ tag, id, classes, attributes, listeners });
+        super({ tag, id, classes: ["layout", ...classes], attributes, listeners });
     }
 
     /* ---------- Root creators ---------- */
@@ -229,6 +229,397 @@ class Layout extends Element {
     }
 }
 
+class ImageEl extends Element {
+    constructor({
+        src,
+        alt = "",
+        id = "",
+        classes = [],
+        attributes = {},
+        listeners = {},
+        lazy = true
+    }) {
+        if (!src) {
+            throw new Error("Image element requires a `src`");
+        }
+
+        super({
+            tag: "img",
+            id,
+            classes: ["image", ...classes],
+            attributes: {
+                src,
+                alt,
+                ...(lazy ? { loading: "lazy" } : {}),
+                ...attributes
+            },
+            listeners
+        });
+
+    }
+}
+
+class Icon extends ImageEl {
+    constructor({
+        src,
+        alt = "",
+        id = "",
+        classes = [],
+        attributes = {},
+        listeners = {},
+        lazy = true
+    }) {
+        super({
+            src,
+            alt,
+            id,
+            classes: ["icon", ...classes],
+            attributes,
+            listeners,
+            lazy
+        });
+
+    }
+}
+
+class Header extends Element {
+    constructor({
+        level = 1,
+        id = "",
+        classes = [],
+        attributes = {},
+        listeners = {},
+    }) {
+        super({
+            tag: "h" + level,
+            id,
+            classes,
+            attributes,
+            listeners
+        });
+    }
+}
+
+class Input extends Element {
+    constructor({
+        type = "text",
+        name = "",
+        value = "",
+        placeholder = "",
+        id = "",
+        classes = [],
+        attributes = {},
+        listeners = {}
+    } = {}) {
+        super({
+            tag: "input",
+            id,
+            classes: ["input", "formEl", ...classes],
+            attributes: {
+                type,
+                name,
+                value,
+                placeholder,
+                ...attributes
+            },
+            listeners
+        });
+    }
+
+    value(val) {
+        if (val === undefined) return this.html.value;
+        this.html.value = val;
+        return this;
+    }
+}
+
+class TextInput extends Input {
+    constructor(options = {}) {
+        super({ ...options, type: "text" });
+    }
+}
 
 
+class EmailInput extends Input {
+    constructor(options = {}) {
+        super({ ...options, type: "email" });
+    }
+}
 
+class PasswordInput extends Input {
+    constructor(options = {}) {
+        super({ ...options, type: "password" });
+    }
+}
+
+class NumberInput extends Input {
+    constructor({
+        type = "text",
+        name = "",
+        value = 0,
+        min = 0,
+        max = 100,
+        placeholder = "",
+        id = "",
+        classes = [],
+        attributes = {},
+        listeners = {}
+    }) {
+        super({ type: "number", name, value, placeholder, id, classes: ["numberInput", ...classes], attributes: { min, max, ...attributes }, listeners });
+    }
+}
+
+class TextArea extends Element {
+    constructor({
+        name = "",
+        value = "",
+        rows = 4,
+        id = "",
+        classes = [],
+        attributes = {}
+    } = {}) {
+        super({
+            tag: "textarea",
+            id,
+            classes: ["textarea", "formEl", ...classes],
+            attributes: {
+                name,
+                rows,
+                ...attributes
+            }
+        });
+
+        this.html.value = value;
+    }
+
+    value(val) {
+        if (val === undefined) return this.html.value;
+        this.html.value = val;
+        return this;
+    }
+}
+
+class Checkbox extends Element {
+    constructor({
+        name = "",
+        checked = false,
+        id = "",
+        classes = [],
+        attributes = {}
+    } = {}) {
+        super({
+            tag: "input",
+            id,
+            classes: ["checkbox", "formEl", ...classes],
+            attributes: {
+                type: "checkbox",
+                name,
+                checked,
+                ...attributes
+            }
+        });
+    }
+
+    checked(val) {
+        if (val === undefined) return this.html.checked;
+        this.html.checked = val;
+        return this;
+    }
+}
+
+class Toggle extends Checkbox {
+
+}
+
+class Select extends Element {
+    constructor({
+        name = "",
+        options = [],
+        value = "",
+        id = "",
+        classes = [],
+        attributes = {}
+    } = {}) {
+        super({
+            tag: "select",
+            id,
+            classes: ["select", "formEl", ...classes],
+            attributes: { name, ...attributes }
+        });
+
+        this.options = options;
+        this.value = value;
+    }
+
+    render() {
+        super.render();
+        this.options.forEach(opt => {
+            const option = document.createElement("option");
+            option.value = opt.value;
+            option.textContent = opt.label;
+            if (opt.value === this.value) option.selected = true;
+            this.html.appendChild(option);
+        });
+    }
+
+    value(val) {
+        if (val === undefined) return this.html.value;
+        this.html.value = val;
+        return this;
+    }
+}
+
+class ToggleButton extends Element {
+    constructor({
+        labelOn = "On",
+        labelOff = "Off",
+        initial = false,
+        id = "",
+        classes = [],
+        attributes = {},
+        listeners = {},
+        onChange = null
+    } = {}) {
+        super({
+            tag: "button",
+            id,
+            classes: ["toggle-button", "formEl", ...classes],
+            attributes: {
+                type: "button",
+                "aria-pressed": initial,
+                ...attributes
+            },
+            listeners
+        });
+
+        this.state = Boolean(initial);
+        this.labelOn = labelOn;
+        this.labelOff = labelOff;
+        this.onChange = onChange;
+    }
+
+    render() {
+        super.render();
+
+        this._renderLabel();
+
+        this.html.addEventListener("click", () => {
+            this.toggle();
+        });
+    }
+
+    _renderLabel() {
+        this.html.textContent = this.state ? this.labelOn : this.labelOff;
+        this.html.setAttribute("aria-pressed", String(this.state));
+        this.html.classList.toggle("is-on", this.state);
+    }
+
+    toggle() {
+        this.state = !this.state;
+        this._renderLabel();
+        if (this.onChange) this.onChange(this.state);
+        return this;
+    }
+
+    value(val) {
+        if (val === undefined) return this.state;
+        this.state = Boolean(val);
+        this._renderLabel();
+        return this;
+    }
+}
+
+class ToggleSwitch extends Element {
+    constructor({
+        checked = false,
+        id = "",
+        classes = [],
+        attributes = {},
+        onChange = null
+    } = {}) {
+        super({
+            tag: "label",
+            classes: ["toggle-switch", "formEl", ...classes]
+        });
+
+        this.input = new Element({
+            tag: "input",
+            attributes: {
+                type: "checkbox",
+                id,
+                ...attributes
+            }
+        });
+
+        this.slider = new Element({
+            tag: "span",
+            classes: ["slider"]
+        });
+
+        this.checked = checked;
+
+        this.onChange = onChange;
+
+        this.append(this.input, this.slider);
+    }
+
+    render() {
+        super.render();
+        this.input.html.checked = this.checked;
+        if (this.onChange) {
+            this.input.html.addEventListener("change", () => {
+                this.onChange(this.input.html.checked);
+            });
+        }
+    }
+
+    value(val) {
+        if (val === undefined) return this.input.html.checked;
+        this.checked = Boolean(val);
+        this.input.html.checked = Boolean(val);
+        return this;
+    }
+
+    toggle() {
+        this.value(!this.value());
+        return this;
+    }
+}
+
+
+class FormGroup extends Layout {
+    constructor({
+        icon = undefined,
+        label = "",
+        input,
+        help = "",
+        error = "",
+        dir = "row"
+    } = {}) {
+        super({
+            classes: [dir, "form-group"]
+        });
+
+        if (label) {
+            this.append(
+                new Header({
+                    level: 3,
+                }).append(...(icon ? [icon, label] : [label]))
+            );
+        }
+
+        if (input) this.append(input);
+
+        if (help) {
+            this.append(
+                new Element({ tag: "small", classes: ["form-help"] }).append(help)
+            );
+        }
+
+        if (error) {
+            this.append(
+                new Element({ tag: "div", classes: ["form-error"] }).append(error)
+            );
+        }
+    }
+}
